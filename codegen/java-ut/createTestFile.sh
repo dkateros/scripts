3f8baf8257c4bee918a60c1324ad46640c106c9e
dkateros <dkateros@gmail.com>
2017-09-26 11:43:40 +0300

    added usage explanation

--- codegen/java-ut/createTestFile.sh
+++ codegen/java-ut/createTestFile.sh
@@ -1,4 +1,12 @@
 #!/bin/sh
+
+if [ $# -lt 1 ]
+then
+        echo "one argument required: the name of the java class"
+        exit
+fi
+
 cd $PWD
+
 java_file=$(find . -name $1".java")
 ctf.py $java_file
